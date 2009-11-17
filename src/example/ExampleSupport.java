285d47c7d991fc29f27bc569d95ec36a1b5b93ef
artur.tomusiak <artur.tomusiak@dcc727fb-27de-444f-bdec-8fd43be206a1>
2009-11-17 17:38:15 +0000

    SMT-4: Moving things around for the build script

--- src/example/ExampleSupport.java
+++ src/example/ExampleSupport.java
@@ -21,41 +21,12 @@
 
 package example;
 
+import com.opensymphony.xwork2.ActionSupport;
+
 /**
- * <code>Set welcome message.</code>
+ * Base Action class for the Tutorial package.
  */
-public class HelloWorld extends ExampleSupport {
-
-    public String execute() throws Exception {
-        setMessage(getText(MESSAGE));
-        return SUCCESS;
-    }
-
-    /**
-     * Provide default valuie for Message property.
-     */
-    public static final String MESSAGE = "HelloWorld.message";
-
-    /**
-     * Field for Message property.
-     */
-    private String message;
-
-    /**
-     * Return Message property.
-     *
-     * @return Message property
-     */
-    public String getMessage() {
-        return message;
-    }
-
-    /**
-     * Set Message property.
-     *
-     * @param message Text to display on HelloWorld page.
-     */
-    public void setMessage(String message) {
-        this.message = message;
-    }
+public class ExampleSupport extends ActionSupport
+{
+    private static final long serialVersionUID = 3515078654701117288L;
 }
